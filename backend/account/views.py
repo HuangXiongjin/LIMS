0915137abb142bda9d23b47445e502b19cffe421
DESKTOP-2CP0C7F\93277 <932777374@qq.com>
2020-06-11 17:30:00 +0800

    no message

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,10 +1,13 @@
 import json
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, make_response
 from flask import Blueprint, jsonify
+from flask_login import login_user
 
 from backend.tools import autocode
 from backend.tools.MyEncode import AlchemyEncoder
 from backend.common.models import Users
+from backend.common.system import db_session, User
+import time, datetime
 
 users = Blueprint('users', __name__)
 
@@ -25,4 +28,30 @@ def make_model():
         except Exception as e:
             print(e)
 
+@users.route('/account/userloginauthentication', methods=['GET', 'POST'])
+def userloginauthentication():
+    '''
+    用户登陆认证
+    :return:
+    '''
+    try:
+        if request.method == 'POST':
+            data = request.values
+            WorkNumber = data.get('WorkNumber')
+            password = data.get('password')
+            # 验证账户与密码
+            user = db_session.query(User).filter_by(WorkNumber=WorkNumber).first()
+            resp = make_response()
+            if user and (user.confirm_password(password) or user.Password == password):
+                login_user(user)  # login_user(user)调用user_loader()把用户设置到db_session中
+                user.session_id = str(time.time())
+                user.LastLoginTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                db_session.commit()
+                return 'OK'
+            else:
+                return '用户名密码错误'
+    except Exception as e:
+        print(e)
+        db_session.rollback()
+        return json.dumps([{"status": "Error:" + str(e)}], cls=AlchemyEncoder, ensure_ascii=False)
 
