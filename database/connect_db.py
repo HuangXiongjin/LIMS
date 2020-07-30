c1aecb92598a9b3d29a4eb725c06a5a265df3129
HuangXiongjin <1975436224@qq.com>
2020-07-30 17:18:38 +0800

    更改了项目结构

---
+++ database/connect_db.py
@@ -0,0 +1,15 @@
+import configparser
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CONFIG_DIR = os.path.join(BASE_DIR, 'config_db.ini')
+config = configparser.ConfigParser()
+config.read(CONFIG_DIR, encoding='utf-8')
+
+USERNAME = config['DATABASE']['USERNAME']
+PASSWORD = config['DATABASE']['PASSWORD']
+HOST = config['DATABASE']['HOST']
+PORT = config['DATABASE']['PORT']
+DB_NAME = config['DATABASE']['DB_NAME']
+
+CONNECT_DATABASE = f"mssql+pymssql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DB_NAME}?charset=utf8"
