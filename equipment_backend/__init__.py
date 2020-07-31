07ff5dfb92d7b9c724708912b75ba00e8633ca86
HuangXiongjin <1975436224@qq.com>
2020-07-31 11:15:41 +0800

    修改了入口文件

--- equipment_backend/__init__.py
+++ equipment_backend/__init__.py
@@ -1,9 +1,7 @@
-from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api, Resource
-from flask import Flask, request
+from flask import Flask
 
 from database.connect_db import CONNECT_DATABASE
 
@@ -14,7 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # 创建数据库连接对象
 db = SQLAlchemy(app)
 
-from account import user_auth
 from equipment_backend.product.equipment_fitting import equipment
 from equipment_backend.product.work_order import work_order
 from common.equipment_models import *
