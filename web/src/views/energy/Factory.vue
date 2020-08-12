405413948e2571df3c4a111498b80401677a110e
ligang <2228695510@qq.ccom>
2020-08-12 14:33:47 +0800

    能耗管理

--- web/src/views/energy/Factory.vue
+++ web/src/views/energy/Factory.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div style="height:1380px;">
         <el-row  :gutter="20">
             <el-col :span="17" style="height:700px;">
                 <div class="marginBottom-10 text-size-18 font_Weight">
@@ -51,13 +51,14 @@
                         <el-col :span='6'>
                             <el-row  style="height:40px;lineHeight:40px;"><el-col :span='24' class="font_Weight text-size-18">区域耗电详情</el-col></el-row>
                             <el-row  style="height:40px;lineHeight:40px;"><el-col :span='24' class="active">汉盟新建综合制剂楼</el-col></el-row>
-                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='6'>年耗电</el-col><el-col :span='6' >21256</el-col><el-col :span='6'>月耗量</el-col><el-col :span='6'>21256</el-col></el-row>
                             <el-row  style="height:40px;lineHeight:40px;"><el-col :span='24' class="font_Weight text-size-18">区域耗电详情</el-col></el-row>
-                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='24' class="font_Weight text-size-18">区域耗电详情</el-col></el-row>
-                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='24' class="font_Weight text-size-18">区域耗电详情</el-col></el-row>
-                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='24' class="font_Weight text-size-18">区域耗电详情</el-col></el-row>
-                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='24' class="font_Weight text-size-18">区域耗电详情</el-col></el-row>
-                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='24' class="font_Weight text-size-18">区域耗电详情</el-col></el-row>
+                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='6' class="brown">年耗电</el-col><el-col :span='6' >21256</el-col><el-col :span='6' class="brown">月耗量</el-col><el-col :span='6'>21256</el-col></el-row>
+                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='6' class="brown">单日量</el-col><el-col :span='6' >1256</el-col><el-col :span='6' class="brown">日均量</el-col><el-col :span='6'>1256</el-col></el-row>
+                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='6' class="brown">变化值</el-col><el-col :span='6' >2.5%</el-col></el-row>
+                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='8' class="brown">当前批次</el-col><el-col :span='16' class="active">JSKGF20200825H</el-col></el-row>
+                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='8' class="brown">生产时长</el-col><el-col :span='16' class="active">03:53:25</el-col></el-row>
+                            <el-row  style="height:30px;lineHeight:30px;"><el-col :span='24' class="brown">内容设备在线情况</el-col></el-row>
+                            <el-row  style="height:40px;lineHeight:40px;"><el-col :span='24' class="active"><el-progress :text-inside="true" :stroke-width="22" :percentage="70"></el-progress></el-col></el-row>
                         </el-col>
                         <el-col :span='18'></el-col>
                     </el-row>
@@ -86,30 +87,75 @@
                      <el-row style="height:35px;lineHeight:35px;"><el-col  :span='8' class="text-size-14">新建综合制剂楼</el-col><el-col :span='5' class="text-size-14">电表2</el-col><el-col :span='5' class="text-size-14">152.2</el-col><el-col :span='6' class="text-size-14">2020-08-05</el-col></el-row>
                      <el-row style="height:35px;lineHeight:35px;"><el-col  :span='8' class="text-size-14">新建综合制剂楼</el-col><el-col :span='5' class="text-size-14">电表2</el-col><el-col :span='5' class="text-size-14">152.2</el-col><el-col :span='6' class="text-size-14">2020-08-05</el-col></el-row>
                      <el-row style="height:35px;lineHeight:35px;"><el-col  :span='8' class="text-size-14">新建综合制剂楼</el-col><el-col :span='5' class="text-size-14">电表2</el-col><el-col :span='5' class="text-size-14">152.2</el-col><el-col :span='6' class="text-size-14">2020-08-05</el-col></el-row>
-                     <el-row style="height:35px;lineHeight:35px;"><el-col  :span='8' class="text-size-14">新建综合制剂楼</el-col><el-col :span='5' class="text-size-14">电表2</el-col><el-col :span='5' class="text-size-14">152.2</el-col><el-col :span='6' class="text-size-14">2020-08-05</el-col></el-row>
                  </div>
             </div>
             </el-col>
     </el-row>
     <el-row style="height:200px;" :gutter="20" class="marginBottom">
-            <el-col :span="8"><div class="scheduleContainer" style="height:200px;">1</div></el-col>
-            <el-col :span="16"><div class="scheduleContainer" style="height:200px;">3</div></el-col>
+            <el-col :span="8">
+                <div class="scheduleContainer" style="height:200px;">
+                    <el-row>
+                         <el-col :span='7'>
+                             <el-row style="height:40px;lineHeight:40px;"><el-col class="text-size-16 font_Weight">批次完成时间</el-col></el-row>
+                             <el-row style="height:30px;lineHeight:30px;"><el-col class="active">年数据</el-col></el-row>
+                             <el-row style="height:30px;lineHeight:30px;"><el-col class="brown">月数据</el-col></el-row>
+                         </el-col>
+                         <el-col :span='17'>雷达图</el-col>
+                     </el-row>
+                </div>
+            </el-col>
+            <el-col :span="16">
+                <div class="scheduleContainer" style="height:200px;">
+                     <el-row>
+                         <el-col :span='6'>
+                             <el-row style="height:40px;lineHeight:40px;"><el-col class="text-size-16 font_Weight">批次完成时间</el-col></el-row>
+                             <el-row style="height:30px;lineHeight:30px;"><el-col class="active">年数据</el-col></el-row>
+                             <el-row style="height:30px;lineHeight:30px;"><el-col class="brown">月数据</el-col></el-row>
+                             <el-row style="height:30px;lineHeight:30px;"><el-col class="brown">实时数据</el-col></el-row>
+                             <el-row style="height:30px;lineHeight:30px;"><el-col class="brown" :span='10'>同比变化</el-col><el-col class="active" :span='12'>-0.56%</el-col></el-row>
+                         </el-col>
+                         <el-col :span='18'>折线图</el-col>
+                     </el-row>
+                </div>
+            </el-col>
     </el-row>
     <el-row style="height:400px;" :gutter="20">
-            <el-col :span="8"><div class="scheduleContainer" style="height:400px;">1</div></el-col>
-            <el-col :span="8"><div class="scheduleContainer" style="height:400px;">3</div></el-col>
+            <el-col :span="8">
+                <div class="scheduleContainer" style="height:400px;">
+                    <el-row style="height:40px;lineHeight:40px;">
+                        <el-col class="text-size-18 font_Weight" >本日耗电量</el-col>
+                        <el-col class="text-size-20 color-orange">1,255,421</el-col>
+                    </el-row>
+                </div>
+            </el-col>
+            <el-col :span="8">
+                <div class="scheduleContainer" style="height:400px;">
+                 <el-row  style="height:40px;lineHeight:40px;">
+                        <el-col class="text-size-18 font_Weight">本日耗电量</el-col>
+                         <el-row><el-col class="active" :span='8'>新建综合制剂楼</el-col><el-col :span='12' class="marginTop12"><el-progress :percentage="percentage" :color="customColor"></el-progress></el-col><el-col class="active" :span='4'>55.3%</el-col></el-row>
+                         <el-row><el-col class="active" :span='8'>新建综合制剂楼</el-col><el-col :span='12' class="marginTop12"><el-progress :percentage="percentage" :color="customColor"></el-progress></el-col><el-col class="active" :span='4'>55.3%</el-col></el-row>
+                         <el-row><el-col class="active" :span='8'>新建综合制剂楼</el-col><el-col :span='12' class="marginTop12"><el-progress :percentage="percentage" :color="customColor"></el-progress></el-col><el-col class="active" :span='4'>55.3%</el-col></el-row>
+                         <el-row><el-col class="active" :span='8'>新建综合制剂楼</el-col><el-col :span='12' class="marginTop12"><el-progress :percentage="percentage" :color="customColor"></el-progress></el-col><el-col class="active" :span='4'>55.3%</el-col></el-row>
+                         <el-row><el-col class="active" :span='8'>新建综合制剂楼</el-col><el-col :span='12' class="marginTop12"><el-progress :percentage="percentage" :color="customColor"></el-progress></el-col><el-col class="active" :span='4'>55.3%</el-col></el-row>
+                         <el-row><el-col class="active" :span='8'>新建综合制剂楼</el-col><el-col :span='12' class="marginTop12"><el-progress :percentage="percentage" :color="customColor"></el-progress></el-col><el-col class="active" :span='4'>55.3%</el-col></el-row>
+                         <el-row><el-col class="active" :span='8'>新建综合制剂楼</el-col><el-col :span='12' class="marginTop12"><el-progress :percentage="percentage" :color="customColor"></el-progress></el-col><el-col class="active" :span='4'>55.3%</el-col></el-row>
+                         <el-row><el-col class="active" :span='8'>新建综合制剂楼</el-col><el-col :span='12' class="marginTop12"><el-progress :percentage="percentage" :color="customColor"></el-progress></el-col><el-col class="active" :span='4'>55.3%</el-col></el-row>
+                 </el-row>
+                </div>
+            </el-col>
             <el-col :span="8"><div class="scheduleContainer" style="height:400px;">3</div></el-col>
     </el-row>
 </div>
 </template>
 <script>
-import G2 from '@antv/g2'
 export default {
     data(){
         return {
             toptab:['电能详情','水能详情','汽能详情'],
             currentkindModel:'电',
             systemActive:'电能详情',
+            percentage: 20,
+            customColor: '#409eff'
         }
     },
     methods:{
@@ -129,5 +175,7 @@ export default {
 .font_Weight{
     font-weight: 700;
 }
-
+.marginTop12{
+    margin-top: 12px;
+}
 </style>
