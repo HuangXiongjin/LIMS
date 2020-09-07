45682a2032f54b8feea5059f14508840d133dd9c
DESKTOP-H0B2FP4\fdd <minwork0926@163.com>
2020-09-07 16:02:13 +0800

    no message

---
+++ web/src/views/scheduling/planOrder.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-row>
+    <el-col>
+      <div class="page-title">
+        <span>生产订单管理</span>
+      </div>
+      <div class="platformContainer">
+
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  export default {
+    name: "planOrder"
+  }
+</script>
+
+<style scoped>
+
+</style>
