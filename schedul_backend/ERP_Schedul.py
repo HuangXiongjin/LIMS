8d36d820c2392bcdb0bb889b1327833aacd70e04
DESKTOP-2CP0C7F\93277 <932777374@qq.com>
2020-09-22 19:52:41 +0800

    no message

--- schedul_backend/ERP_Schedul.py
+++ schedul_backend/ERP_Schedul.py
@@ -474,17 +474,24 @@ def addEquipmentBatchRunTime():
                     PUCode = pl.get("PUCode")
                     eqList = pl.get('eqList')
                     for el in eqList:
-                        ert = EquipmentBatchRunTime()
-                        ert.BatchID = BatchID
-                        ert.EQPCode = el.get("EQPCode")
-                        ert.EQPName = el.get("EQPName")
-                        ert.PUCode = PUName
-                        ert.PUName = PUCode
-                        # ert.StartTime = el.get("")
-                        # ert.EndTime = el.get("")
-                        ert.WorkTime = el.get("workTime")
-                        ert.WaitTime = el.get("waitTime")
-                        db_session.add(ert)
+                        isSelected = el.get("isSelected")
+                        if isSelected == True:
+                            ert = EquipmentBatchRunTime()
+                            ert.BatchID = BatchID
+                            ert.EQPCode = el.get("EQPCode")
+                            ert.EQPName = el.get("EQPName")
+                            ert.PUCode = PUName
+                            ert.PUName = PUCode
+                            # ert.StartTime = el.get("")
+                            # ert.EndTime = el.get("")
+                            ert.WorkTime = el.get("workTime")
+                            ert.WaitTime = el.get("waitTime")
+                            db_session.add(ert)
+                        else:
+                            ebrt = db_session.query(EquipmentBatchRunTime).filter(EquipmentBatchRunTime.BatchID == BatchID,
+                                       EquipmentBatchRunTime.EQPCode == el.get("EQPCode"), EquipmentBatchRunTime.PUCode == pl.get("PUCode")).first()
+                            if ebrt:
+                                db_session.delete(ebrt)
                     db_session.commit()
             return json.dumps({"code": "200", "message": "保存成功！", "data": "OK"})
         except Exception as e:
