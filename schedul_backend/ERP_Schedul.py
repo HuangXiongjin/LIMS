8e579441ab34f091ea88889082695ad085b07a97
DESKTOP-2CP0C7F\93277 <932777374@qq.com>
2020-09-23 09:34:27 +0800

    no message

--- schedul_backend/ERP_Schedul.py
+++ schedul_backend/ERP_Schedul.py
@@ -492,7 +492,7 @@ def addEquipmentBatchRunTime():
                                        EquipmentBatchRunTime.EQPCode == el.get("EQPCode"), EquipmentBatchRunTime.PUCode == pl.get("PUCode")).first()
                             if ebrt:
                                 db_session.delete(ebrt)
-                oclass.PlanStatus = Global.PlanStatus.Check.value
+                oclass.PlanStatus = Global.PlanStatus.Confirm.value
                 db_session.commit()
             return json.dumps({"code": "200", "message": "保存成功！", "data": "OK"})
         except Exception as e:
