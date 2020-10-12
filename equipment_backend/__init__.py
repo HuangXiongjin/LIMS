4077350b31ad5b7f24ad8b168e58170b415b13c1
HuangXiongjin <likehxj@qq.com>
2020-10-12 14:59:55 +0800

    修改了启动文件

--- equipment_backend/__init__.py
+++ equipment_backend/__init__.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = CONNECT_DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# 创建数据库连接对象
 db = SQLAlchemy(app)
 
 from equipment_backend.product.equipment_fitting import equipment
@@ -19,13 +18,11 @@ from common.equipment_models import *
 app.register_blueprint(equipment, url_prefix='/equipment')
 app.register_blueprint(work_order, url_prefix='/work')
 
-# 绑定app和数据库，迁移使用
 migrate = Migrate(app, db)
-# 传输命令行参数
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello LIMS!!!'
+    return 'This is LIMS!'
