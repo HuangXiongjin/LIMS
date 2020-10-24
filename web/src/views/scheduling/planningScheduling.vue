ae2184b8273b76d9f9bedb0939851ec7d57f97d3
ligang <2228695510@qq.ccom>
2020-10-24 16:09:10 +0800

    多批次

--- web/src/views/scheduling/planningScheduling.vue
+++ web/src/views/scheduling/planningScheduling.vue
@@ -539,6 +539,7 @@ var moment=require('moment')
         inProcessList:[],//存储工序设备集合
         BatchWeight:'200片',
         row:{},
+        datalist:[],
         batchtableconfig:[{prop:'PlanNum',label:"计划单号"},{prop:'BatchID',label:'批次号'},{prop:'BrandCode',label:'品名编码'},{prop:'BrandName',label:'品名'},{prop:'BrandType',label:'产品类型'},{prop:'PlanQuantity',label:'每批产量'},{prop:'Unit',label:'单位'},{prop:'PlanStatus',label:'计划状态'}],//批次列表
         eqlistableconfig:[{prop:'PlanNum',label:"计划单号"},{prop:'BatchID',label:'批次号'},{prop:'BrandCode',label:'品名编码'},{prop:'BrandName',label:'品名'},{prop:'BrandType',label:'产品类型'},{prop:'PlanQuantity',label:'每批产量'},{prop:'Unit',label:'单位'},{prop:'PlanStatus',label:'计划状态'}],//选择设备列表
         tipstableconfig:[{prop:'BatchID',label:"冲突批次号"},{prop:'BrandName',label:'冲突品名'},{prop:'EQPName',label:'冲突设备名称'},{prop:'EQPCode',label:'冲突设备编码'},{prop:'StartTime',label:'冲突开始运行时间'},{prop:'EndTime',label:'冲突结束运行时间'},{prop:'StartBC',label:'冲突开始运行班次'},{prop:'EndBC',label:'冲突结束运行班次'}],//冲突列表
@@ -883,7 +884,22 @@ var moment=require('moment')
         this.$refs.chmultipleTable.toggleRowSelection(e)
       },
       getAllbatchrow(e){ //审核计划批次点击
-        console.log(e)
+        var arr=e
+        this.datalist=arr.map((item) => {
+          return {
+            PlanStatus:'待配置',
+            Describtion:'',
+            ID:item.ID
+          }
+        })
+      },
+      shMultiplebatch(){
+        var params={
+          datalist:this.datalist
+        }
+        this.axios.post('/api/checkPlanManager',this.qs.stringify(params)).then((res) => {
+           console.log(res)
+         })
       },
       xfHandleSizeChange(limit){ //下发批次计划 每页条数切换
         this.xfTableData.limit = limit
