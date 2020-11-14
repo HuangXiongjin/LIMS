94ade3fc9b71751b4c8b3950a89d8f0b6dcda135
HuangXiongjin <likehxj@qq.com>
2020-11-14 14:46:39 +0800

    解决lims跨域

--- lims_run.py
+++ lims_run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from database.connect_db import CONNECT_DATABASE
 from lims_backend.test import t1
@@ -7,8 +8,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = CONNECT_DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.register_blueprint(t1, url_prefix='/work')
+
 # app.register_blueprint(work_order, url_prefix='/work')
+
+
+def create_app():
+    CORS(app, supports_credentials=True)
+    app.register_blueprint(t1, url_prefix='/work')
+    return app
 
 
 @app.route('/')
