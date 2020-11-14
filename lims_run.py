dfbffc65648b827c25505d2b17f4b1ff4d88e1b0
ligang <2228695510@qq.ccom>
2020-11-14 14:57:26 +0800

    port10002

--- lims_run.py
+++ lims_run.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 from database.connect_db import CONNECT_DATABASE
-from lims_backend.test import t1
+# from lims_backend.test import t1
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = CONNECT_DATABASE
@@ -14,6 +14,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 def create_app():
     CORS(app, supports_credentials=True)
+    from lims_backend.test import t1
+
     app.register_blueprint(t1, url_prefix='/work')
     return app
 
