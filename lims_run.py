314f139d5dbd66d9e7e1246dad4a5e90853ab0ff
HuangXiongjin <likehxj@qq.com>
2020-12-02 16:21:41 +0800

    修改了登录

--- lims_run.py
+++ lims_run.py
@@ -4,11 +4,14 @@ from flask_cors import CORS
 from database.connect_db import CONNECT_DATABASE
 # from lims_backend.test import t1
 from lims_backend.system_api import system_interface
+from system_backend.SystemManagement import account_auth
 from system_backend.SystemManagement.account_auth import login_auth
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = CONNECT_DATABASE
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '1qaz2wsx3edd45'
+account_auth.login_manager.init_app(app)
 
 CORS(app, supports_credentials=True)
 # app.register_blueprint(t1, url_prefix='/work')
