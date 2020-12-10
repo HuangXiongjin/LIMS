9e22a3620efe564e607d252e52e2a6ba8917c965
HuangXiongjin <likehxj@qq.com>
2020-12-10 10:22:33 +0800

    添加了样品分发接口

--- lims_backend/distribute_api.py
+++ lims_backend/distribute_api.py
@@ -12,13 +12,14 @@ distribute = Blueprint('distribute', __name__)
 
 @distribute.route('/Distribute', methods=['POST'])
 def product_distribute():
+    """样品分发"""
     CheckProjectNO = request.values.get('CheckProjectNO')
     User = request.values.get('User')
-    Number = request.values.get('Number')
+    # Number = request.values.get('Number')
     Group = request.values.get('Group')
     GroupUser = request.values.get('GroupUser')
     Time = request.values.get('GroupUser')
-    db_session.add(Distribute(CheckProjectNO=CheckProjectNO, User=User, Number=Number, Group=Group, GroupUser=GroupUser,
+    db_session.add(Distribute(CheckProjectNO=CheckProjectNO, User=User, Group=Group, GroupUser=GroupUser,
                               Time=Time))
     db_session.commit()
     return json.dumps({'code': '1000', 'msg': '操作成功'}, ensure_ascii=False)
